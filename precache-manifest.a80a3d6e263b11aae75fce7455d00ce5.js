0bcc22317bb1902020cd941c8bb635db0a639b69
NSarg7 <NSarg7@users.noreply.github.com>
2020-03-19 01:26:13 +0400

    Updates

---
+++ precache-manifest.a80a3d6e263b11aae75fce7455d00ce5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d29d30d62f30fe6db014df33b3cc6d",
+    "url": "/pizza-maker/index.html"
+  },
+  {
+    "revision": "81a5546e3c2091263216",
+    "url": "/pizza-maker/static/css/main.5afd15c4.chunk.css"
+  },
+  {
+    "revision": "3633717b510a4a7e6eef",
+    "url": "/pizza-maker/static/js/2.acfab2a3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/pizza-maker/static/js/2.acfab2a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a5546e3c2091263216",
+    "url": "/pizza-maker/static/js/main.7f1e5500.chunk.js"
+  },
+  {
+    "revision": "77803edc2dd16140d722",
+    "url": "/pizza-maker/static/js/runtime-main.38aae517.js"
+  },
+  {
+    "revision": "14d60239e09e72f4d44b305b68574bc1",
+    "url": "/pizza-maker/static/media/1.14d60239.png"
+  },
+  {
+    "revision": "951d7f8d8eb4b6bedfb30cf8d52b02de",
+    "url": "/pizza-maker/static/media/back-min.951d7f8d.jpg"
+  },
+  {
+    "revision": "0d7b837f2799025d0422a5be2ba93731",
+    "url": "/pizza-maker/static/media/cutting-board.0d7b837f.png"
+  },
+  {
+    "revision": "14e573daf034211e74566bd17e17dfbb",
+    "url": "/pizza-maker/static/media/greens.14e573da.png"
+  },
+  {
+    "revision": "547b4971dbcf87bb60c519a79a3cc1c2",
+    "url": "/pizza-maker/static/media/greens.547b4971.png"
+  },
+  {
+    "revision": "d6ff94b87e43fba4b42477c473f5bd62",
+    "url": "/pizza-maker/static/media/mashrooms.d6ff94b8.png"
+  },
+  {
+    "revision": "e3e28b266ee2d86112e0eb45a2fb11a7",
+    "url": "/pizza-maker/static/media/mashrooms.e3e28b26.png"
+  },
+  {
+    "revision": "64bf3818bf23586486671ce68e43eae8",
+    "url": "/pizza-maker/static/media/olive.64bf3818.png"
+  },
+  {
+    "revision": "6c9efc12fca5057dcbcd0aa86b6efe4d",
+    "url": "/pizza-maker/static/media/olive.6c9efc12.png"
+  },
+  {
+    "revision": "53b99974f00a54f36944caed554e9baa",
+    "url": "/pizza-maker/static/media/pizza.53b99974.png"
+  },
+  {
+    "revision": "7c28f7446e97c132152572bbd8c427d9",
+    "url": "/pizza-maker/static/media/sausage.7c28f744.png"
+  },
+  {
+    "revision": "dc174a6c13d99c33a408e71169ea4dc6",
+    "url": "/pizza-maker/static/media/sausage.dc174a6c.png"
+  },
+  {
+    "revision": "1bbe7bfcd3943c9424572216e791a94b",
+    "url": "/pizza-maker/static/media/tomato.1bbe7bfc.png"
+  },
+  {
+    "revision": "513a9f2e6c55c35c01751e41a273e29c",
+    "url": "/pizza-maker/static/media/tomato.513a9f2e.png"
+  }
+]);
